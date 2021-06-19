02130ecde37035d0ed6d9bb666c6268d855d4cd8
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-19 13:53:50 +0530

    conditional-error-msg

--- user-form/main.js
+++ user-form/main.js
@@ -8,10 +8,18 @@ form.addEventListener('submit',onSubmit)
 
 function onSubmit(e) {
   e.preventDefault()
+
   const name = nameInput.value
   const email = emailInput.value
+
   if(!name || !email) {
-    errorMessage.classList.add('error')
-    errorMessage.textContent = 'Enter the details'
+  errorMessage.classList.add('error')
+  errorMessage.textContent = 'Enter the details'
+
+    setTimeout(() => {
+      errorMessage.classList.remove('error')
+      errorMessage.textContent = ''
+    },2000)
+
   }
 }
