03a26df084edcb2e278c837826236c396d3ff9b5
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-19 13:15:11 +0530

    basics-of-querySelector

---
+++ user-form/main.js
@@ -0,0 +1,9 @@
+const list = document.querySelector('.items')
+const btn = document.querySelector('.btn')
+
+//list.remove()
+//list.lastElementChild.remove()
+//list.firstElementChild.textContent = 'hello binay'
+//list.children[0].innerText = 'bin'
+//list.children[1].innerHTML = '<h1>Binay</h1>'
+// btn.style.color='red'
