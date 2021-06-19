d2da7887bb3e04326497af676597ec065b797e57
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-19 19:39:12 +0530

    item-lister - item-delete

--- item-lister/main.js
+++ item-lister/main.js
@@ -1,8 +1,9 @@
 const form = document.querySelector('#addForm')
 const itemInput = document.querySelector('#item')
-const list = document.querySelector('.list-group')
+const itemList = document.querySelector('#items')
 
 form.addEventListener('submit',onSubmit)
+itemList.addEventListener('click',removeItem)
 
 function onSubmit(e) {
   e.preventDefault()
@@ -11,10 +12,11 @@ function onSubmit(e) {
 
   if(!itemText) {
     window.alert('Enter the item')
+    return
   }
 
   const removeButton = document.createElement('button')
-  removeButton.innerText = 'X'
+  removeButton.appendChild(document.createTextNode('X'))
   removeButton.setAttribute('class','btn btn-danger btn-sm float-right delete')
 
   const itemLi = document.createElement('li')
@@ -22,7 +24,14 @@ function onSubmit(e) {
   itemLi.appendChild(removeButton)
   itemLi.setAttribute('class','list-group-item')
 
-  list.appendChild(itemLi)
+  itemList.appendChild(itemLi)
 
-  console.log(list)
+  console.log(itemList)
+}
+
+function removeItem(e) {
+  if(e.target.classList.contains('delete')) {
+    e.target.parentElement.remove()
+  }
+
 }
